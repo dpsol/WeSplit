80112816835023fffe208b8112630d0c0476b5bc
Daniel Pérez Solís <dpsol2008@gmail.com>
2020-07-01 09:06:32 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Daniel Pérez Solís on 01/07/20.
+//  Copyright © 2020 Daniel Pérez Solís. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
